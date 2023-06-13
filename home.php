7a47a8f8866a724237e8cd8c44debbf66222f7ee
GaneshChintalapudi <113678047+GaneshChintalapudi@users.noreply.github.com>
2023-06-13 12:23:51 +0530

    Add files via upload

--- home.php
+++ home.php
@@ -1,5 +1,4 @@
-<?php include('db_connect.php'); ?>
-<?php 
+<?php include('db_connect.php');
 function ordinal_suffix1($num){
     $num = $num % 100; // protect against large numbers
     if($num < 11 || $num > 13){
@@ -13,7 +12,8 @@ function ordinal_suffix1($num){
 }
 $astat = array("Not Yet Started","On-going","Closed");
  ?>
- <div class="col-12">
+
+<div class="col-12">
     <div class="card">
       <div class="card-body">
         Welcome <?php echo $_SESSION['login_name'] ?>!
@@ -27,53 +27,3 @@ $astat = array("Not Yet Started","On-going","Closed");
       </div>
     </div>
 </div>
-        <div class="row">
-          <div class="col-12 col-sm-6 col-md-4">
-            <div class="small-box bg-light shadow-sm border">
-              <div class="inner">
-                <h3><?php echo $conn->query("SELECT * FROM faculty_list ")->num_rows; ?></h3>
-
-                <p>Total Faculties</p>
-              </div>
-              <div class="icon">
-                <i class="fa fa-user-friends"></i>
-              </div>
-            </div>
-          </div>
-           <div class="col-12 col-sm-6 col-md-4">
-            <div class="small-box bg-light shadow-sm border">
-              <div class="inner">
-                <h3><?php echo $conn->query("SELECT * FROM student_list")->num_rows; ?></h3>
-
-                <p>Total Students</p>
-              </div>
-              <div class="icon">
-                <i class="fa ion-ios-people-outline"></i>
-              </div>
-            </div>
-          </div>
-           <div class="col-12 col-sm-6 col-md-4">
-            <div class="small-box bg-light shadow-sm border">
-              <div class="inner">
-                <h3><?php echo $conn->query("SELECT * FROM users")->num_rows; ?></h3>
-
-                <p>Total Users</p>
-              </div>
-              <div class="icon">
-                <i class="fa fa-users"></i>
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-sm-6 col-md-4">
-            <div class="small-box bg-light shadow-sm border">
-              <div class="inner">
-                <h3><?php echo $conn->query("SELECT * FROM class_list")->num_rows; ?></h3>
-
-                <p>Total Classes</p>
-              </div>
-              <div class="icon">
-                <i class="fa fa-list-alt"></i>
-              </div>
-            </div>
-          </div>
-      </div>
--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,7 @@
     </a>
       
     </div>
-    <div class="sidebar">
+    <div class="sidebar ">
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column nav-flat" data-widget="treeview" role="menu" data-accordion="false">
          <li class="nav-item dropdown">
@@ -18,122 +18,13 @@
             </a>
           </li>
           <li class="nav-item dropdown">
-            <a href="./index.php?page=subject_list" class="nav-link nav-subject_list">
+            <a href="./index.php?page=result" class="nav-link nav-result">
               <i class="nav-icon fas fa-th-list"></i>
               <p>
-                Subjects
+                Evaluation Result
               </p>
             </a>
           </li> 
-          <li class="nav-item dropdown">
-            <a href="./index.php?page=class_list" class="nav-link nav-class_list">
-              <i class="nav-icon fas fa-list-alt"></i>
-              <p>
-                Classes
-              </p>
-            </a>
-          </li> 
-          <li class="nav-item dropdown">
-            <a href="./index.php?page=academic_list" class="nav-link nav-academic_list">
-              <i class="nav-icon fas fa-calendar"></i>
-              <p>
-                Acadamic Year
-              </p>
-            </a>
-          </li> 
-          <li class="nav-item dropdown">
-            <a href="./index.php?page=questionnaire" class="nav-link nav-questionnaire">
-              <i class="nav-icon fas fa-file-alt"></i>
-              <p>
-                Questionnaires
-              </p>
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a href="./index.php?page=criteria_list" class="nav-link nav-criteria_list">
-              <i class="nav-icon fas fa-list-alt"></i>
-              <p>
-                Evaluation Critria
-              </p>
-            </a>
-          </li> 
-          <li class="nav-item">
-            <a href="#" class="nav-link nav-edit_faculty">
-              <i class="nav-icon fas fa-user-friends"></i>
-              <p>
-                Faculties
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.php?page=new_faculty" class="nav-link nav-new_faculty tree-item">
-                  <i class="fas fa-angle-right nav-icon"></i>
-                  <p>Add New</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index.php?page=faculty_list" class="nav-link nav-faculty_list tree-item">
-                  <i class="fas fa-angle-right nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link nav-edit_student">
-              <i class="nav-icon fa ion-ios-people-outline"></i>
-              <p>
-                 Students
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.php?page=new_student" class="nav-link nav-new_student tree-item">
-                  <i class="fas fa-angle-right nav-icon"></i>
-                  <p>Add New</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index.php?page=student_list" class="nav-link nav-student_list tree-item">
-                  <i class="fas fa-angle-right nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item dropdown">
-            <a href="./index.php?page=report" class="nav-link nav-report">
-              <i class="nav-icon fas fa-list-alt"></i>
-              <p>
-                Evaluation Report
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link nav-edit_user">
-              <i class="nav-icon fas fa-users"></i>
-              <p>
-                Users
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.php?page=new_user" class="nav-link nav-new_user tree-item">
-                  <i class="fas fa-angle-right nav-icon"></i>
-                  <p>Add New</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index.php?page=user_list" class="nav-link nav-user_list tree-item">
-                  <i class="fas fa-angle-right nav-icon"></i>
-                  <p>List</p>
-                </a>
-              </li>
-            </ul>
-          </li>
         </ul>
       </nav>
     </div>
